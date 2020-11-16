a996b99bdb8545cbf9a5746d2e7c3c9957b4d70d
Max <maliarmaximilian@gmail.com>
2020-11-16 22:45:31 +0100

    added modal

---
+++ assets/vue/editModal.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- Modal -->
+<div class="modal fade" id="editModal" data-backdrop="static" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="editModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import {Modal} from "bootstrap"
+export default {
+	
+	mounted() {
+		console.log(Modal)
+		console.log(document.getElementById('editModal'));
+		this.Modal = new Modal(document.getElementById('editModal'))
+	},
+	data: function () {
+		return {
+			Modal: null
+		}
+	},
+	methods: {
+		hide: function() {
+			this.Modal.hide();
+		},
+		show: function() {
+			this.Modal.show();
+		},
+	}
+}
+</script>
+
+<style>
+
+</style>
--- assets/vue/index.vue
+++ assets/vue/index.vue
@@ -1,36 +1,46 @@
 <script>
-import FullCalendar from '@fullcalendar/vue'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from '@fullcalendar/interaction'
+import FullCalendar from "@fullcalendar/vue";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import EditModal from "./editModal.vue";
 
 export default {
   components: {
-    FullCalendar // make the <FullCalendar> tag available
+    FullCalendar, // make the <FullCalendar> tag available
+    EditModal,
   },
   data() {
     return {
       calendarOptions: {
-        plugins: [ dayGridPlugin, interactionPlugin ],
-        initialView: 'dayGridMonth',
-        dateClick: this.handleDateClick,
+        plugins: [dayGridPlugin, interactionPlugin],
+        initialView: "dayGridMonth",
+		dateClick: this.handleDateClick,
+		eventClick: this.handleEventClick,
         events: [
-          { title: 'event 1', date: '2020-11-01' },
-          { title: 'event 2', date: '2020-11-02' }
-        ]
-      }
-    }
+          { title: "event 1", date: "2020-11-01", color: '#ff0000', editable: true},
+          { title: "event 2", date: "2020-11-02", display: 'background', backgroundColor: '#ff0000', editable: true},
+        ],
+      },
+    };
   },
   methods: {
-    handleDateClick: function(arg) {
-	  alert('date click! ' + arg.dateStr)
-	  console.log(arg)
-    }
-  }
-}
+    handleDateClick: function (arg) {
+      this.$refs.editModal.show();
+      console.log(arg);
+	},
+	handleEventClick: function (arg) {
+      this.$refs.editModal.show();
+      console.log(arg);
+    },
+  },
+};
 </script>
+
 <template>
-<div>
-<FullCalendar :options="calendarOptions" />
-</div>
-  
+  <div>
+    <EditModal ref="editModal" />
+    <div class="m-5">
+      <FullCalendar :options="calendarOptions" />
+    </div>
+  </div>
 </template>
