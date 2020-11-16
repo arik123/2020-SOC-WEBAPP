d6a498ff0cba60c005ef1c33072343fb14c6bb42
Max <maliarmaximilian@gmail.com>
2020-11-16 21:46:22 +0100

    added bootstrap, vue, calendar

---
+++ assets/index.js
@@ -0,0 +1,17 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+import Vue from 'vue'
+import index from './vue/index.vue'
+
+new Vue({
+	render: h => h(index)
+  }).$mount(
+	document.getElementById("app")
+  )
---
+++ assets/vue/index.vue
@@ -0,0 +1,36 @@
+<script>
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [ dayGridPlugin, interactionPlugin ],
+        initialView: 'dayGridMonth',
+        dateClick: this.handleDateClick,
+        events: [
+          { title: 'event 1', date: '2020-11-01' },
+          { title: 'event 2', date: '2020-11-02' }
+        ]
+      }
+    }
+  },
+  methods: {
+    handleDateClick: function(arg) {
+	  alert('date click! ' + arg.dateStr)
+	  console.log(arg)
+    }
+  }
+}
+</script>
+<template>
+<div>
+<FullCalendar :options="calendarOptions" />
+</div>
+  
+</template>
